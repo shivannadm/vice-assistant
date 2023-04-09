934f7c7f42ccca9e513bfbf31a3cc3a6dd8d65c2
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-04-09 22:55:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -109,3 +109,5 @@ if __name__ == "__main__":
             except Exception as e:
                 print(e)
                 speak("I am not able to send the Email , please try again ")
+
+                
